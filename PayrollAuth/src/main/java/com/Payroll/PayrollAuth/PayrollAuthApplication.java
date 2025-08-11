9439a74719140940ad60e204c3d73ef22de30cdf
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2025-08-11 23:44:14 +0530

    added payrollAuth microservice

---
+++ PayrollAuth/src/main/java/com/Payroll/PayrollAuth/PayrollAuthApplication.java
@@ -0,0 +1,13 @@
+package com.Payroll.PayrollAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollAuthApplication.class, args);
+	}
+
+}
---
+++ PayrollAuth/src/test/java/com/Payroll/PayrollAuth/PayrollAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Payroll.PayrollAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
