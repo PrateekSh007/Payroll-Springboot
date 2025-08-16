ca4fe9d65f61372447dee98988147090a441e9c9
PrateekSharma007 <btech.m2_66995@apsc.mait.ac.in>
2025-08-16 19:40:59 +0530

    entity for salary

---
+++ PayrollAuth/src/main/java/com/Payroll/PayrollAuth/entity/salary.java
@@ -0,0 +1,31 @@
+package com.Payroll.PayrollAuth.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class salary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id ;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    private Double baseSalary;
+    private Double allowance;
+    private Double deductions;
+    private Double netSalary;
+
+    private LocalDate month ;
+}
